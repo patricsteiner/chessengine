8339a009b516fc2719ca6e7c9bce33c63b956011
Patric Steiner <patric.steiner1@gmail.com>
2020-08-07 01:22:55 +0200

    setup empty sprint boot project 🕺

---
+++ src/main/kotlin/io/github/patricsteiner/chessengine/ChessengineApplication.kt
@@ -0,0 +1,11 @@
+package io.github.patricsteiner.chessengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChessengineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChessengineApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/patricsteiner/chessengine/ChessengineApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.patricsteiner.chessengine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChessengineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
